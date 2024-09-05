d0baeaf6f8bd6a8c98bdda811f1d62cb34e7d494
Aritra Dey <adey01027@gmail.com>
2024-09-06 01:53:45 +0530

    added loop and epect

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,29 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the Number");
+
+    // Generate a random number between 1 and 100
+    let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
+    loop {
+        println!("Please input your guess");
+
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read user input");
+
+        // Parse the input guess as a u32
+        let guess: u32 = guess.trim().parse().expect("failed to parse");
+        match guess.cmp(&secret_number) {
+            Ordering::Equal => {
+                println!("you won");
+                break;
+            }
+            Ordering::Less => println!("too small"),
+            Ordering::Greater => println!("too big"),
+        }
+        println!("You guessed: {guess}");
+    }
 }
